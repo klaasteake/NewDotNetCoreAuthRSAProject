dee79451eb62ce071f335ea7feca9d3991bf7946
Gino Bos <GinoB@minor.infosupport.edu>
2016-12-22 15:39:00 +0100

    proberen token op te halen... Lukt helaas nog niet.

--- NGAuthApp/src/client/app/home/home.component.ts
+++ NGAuthApp/src/client/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable  } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Http } from '@angular/http';
 import { User } from '../shared/user';
+import { contentHeaders } from '../shared/headers';
+
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -12,6 +14,8 @@ import { User } from '../shared/user';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
+
+@Injectable()
 export class HomeComponent implements OnInit {
 
   loginForm : FormGroup;
@@ -33,13 +37,28 @@ export class HomeComponent implements OnInit {
   }
 
   login() {
-    var user : User =  new User(this.username, this.password);
-    
+
+    var body = JSON.stringify({"username": this.username, "password": this.password});
+
+    this.http.post('http://localhost:3653/token', body, { headers: contentHeaders })
+      .subscribe(
+        response => {
+          console.log(response.json().id_token);
+          localStorage.setItem('id_token', response.json().id_token);
+
+        },
+        error => {
+          console.log("errors:", error);
+        }
+      );
+      
+    /*
     this.http.get('http://localhost:3653/api/login/5').subscribe(response => {
       this.statuscode = response.status,
       this.statustext = response.statusText,
       this.result = response.json();
     }, (errors) => {this.statustext = errors, this.result = "No data received"});
+    */
   }
 
   getPublicData() {
